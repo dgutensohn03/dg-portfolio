747b8389aff332448ff87bb5b66bb95bb5f017a9
Daniel <dgutensohn03@yahoo.com>
2025-10-14 13:44:37 -0600

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// vite.config.ts
 export default defineConfig({
-  base: '/dg-portfolio/', // ✅ must match your repo name EXACTLY
+  base: process.env.NODE_ENV === "production" ? "/dg-portfolio/" : "/", // ✅ matches repo name exactly
   plugins: [react(), tailwindcss()]
 });
